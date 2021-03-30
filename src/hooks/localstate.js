3ba63591d98914a611c995d686e2e6fa40319922
Seb Ringrose <sebringrose@gmail.com>
2021-03-30 11:24:29 +0100

    useLocalState hook working

--- src/hooks/localstate.js
+++ src/hooks/localstate.js
@@ -1,13 +1,24 @@
 import { useState, useEffect } from "https://cdn.skypack.dev/preact/hooks"
 
-const useLocalState = (name, value) => {
-    const [state, setState] = useState(value)
+// This hook will create a state variable and record it's value
+// to localStorage anytime it is set.
+//
+// The provided value is used for initialization if there is no 
+// existing value for matching key in localStorage.
+//
+// The provided value will also be used in the server-side render
+// therefore making it the initial state on load.
 
-    // return early if no localStorage (server-side renders)
-    if (typeof localStorage === "undefined") return [state, setState]
+const useLocalState = (name, value) => {
+    // return regular state if no localStorage (server-side renders)
+    if (typeof localStorage === "undefined") return useState(value)
+
+    let localState = localStorage.getItem(name)
+
+    const [state, setState] = useState(localState ? JSON.parse(localState) : value)
 
     useEffect(() => {
-        localStorage.setItem(name, state)
+        localStorage.setItem(name, JSON.stringify(state))
     }, [state])
 
     return [state, setState]
