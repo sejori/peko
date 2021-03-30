920b73a59c83b7e6f3eaaada2f8e813c99c18bdc
Seb Ringrose <sebringrose@gmail.com>
2021-03-30 16:27:45 +0100

    documenting & moved server.js to lib

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# velocireno
+# velocireno - Frontend Web Framework
 
-Start: `$ deno run --allow-net --allow-read --allow-env --unstable bin/server.js`
+## The epic union of Deno 🦕, Skypack 🛰️ & Preact ⚛️
+
+A deno http server renders the content of the page and serves it to the browser as html. The browser then fetches the page's Javascript via Skypack CDN (for external modules) and the Deno server (for custom modules) and hydrates the page.
+
+## Why is this cool?
+
+Because there is no transpilation of Javascript required. The deno server and the browser use the exact same code!
+
+No code bundling is required either and by using es modules we can leverage the browsers in-built caching systems to keep our sites as speedy as your favourite chicken-looking dinosaur or blue-coloured hedgehog.
+
+## Commands:
+
+Start: `$ deno run --allow-net --allow-read --allow-env --unstable lib/server.js`
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,14 @@ import App from "../components/App.js"
 const Home = () => {
     return html`
         <${Layout} navColour="palegreen">
-            <h1>The epic union of Deno & Preact 💖</h1>
-            <p>A deno http server has rendered the content of this page and served it to your browser as html.</p>
-            <p>Your browser then fetchs the Preact Javascript module for the page (along with all of it's imports) and hydrated the page.</p>
-            <p>To see this in action refresh the page and watch the last render time below...</p> 
+            <h1>The epic union of Deno 🦕, Skypack 🛰️ & Preact ⚛️</h1>
+            <p>A deno http server renders the content of this page and serves it to the browser as html. The browser then fetches the page's Javascript via Skypack CDN (for external modules) and the Deno server (for custom modules) and hydrates the page.</p>
+            <p>To see this in action refresh the page and watch the last render time below.</p> 
             <${App} />
             <p>Last render: ${new Date().toString()}</p>
+            <h2>Why is this cool?</h2>
+            <p>Because there is no transpilation of Javascript required. The deno server and the browser use the exact same code!</p>
+            <p>No code bundling is required either and by using es modules we can leverage the browsers in-built caching systems to keep our sites as speedy as your favourite chicken-looking dinosaur or blue-coloured hedgehog.</p>
         </${Layout}>
     `
 }
