46a0914f09203add73873c5f1ebd5fc743f5cb95
Seb Ringrose <sebringrose@gmail.com>
2021-04-07 13:03:56 +0100

    prod bundling set-up but breaking in about page

--- src/templates/default.js
+++ src/templates/default.js
@@ -1,11 +1,12 @@
-export default ({ path, css, html, script }) => `
+export default ({ path, css, script, html }) => `
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Velocireno 🐓 ${path !== "/" ? `| ${path.slice(1,2).toUpperCase()}${path.slice(2)}` : ""}</title>
+        <link rel="icon" href="/assets/favicon.ico">
+        <title>Velocireno ${path !== "/" ? `| ${path.slice(1,2).toUpperCase()}${path.slice(2)}` : ""}</title>
         <meta name="description" content="A simple, monolithic web application framework for Deno.">
         <meta name="keywords" content="site, description">
         <style>
@@ -13,12 +14,10 @@ export default ({ path, css, html, script }) => `
         </style>
     </head>
     <body>
+        ${script}
         <div id="root">
             ${html}
         </div>
-        <script type="module" defer preload>
-            ${script}
-        </script>
     </body>
     </html>
 `
