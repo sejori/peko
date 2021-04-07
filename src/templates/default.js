506820d37314175a7d83aca6c17f4bbe59015ba6
Seb Ringrose <sebringrose@gmail.com>
2021-04-07 16:40:51 +0100

    🐳

--- src/templates/default.js
+++ src/templates/default.js
@@ -1,4 +1,4 @@
-export default ({ path, css, script, html }) => `
+export default ({ request, css, script, html }) => `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -6,8 +6,8 @@ export default ({ path, css, script, html }) => `
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="/assets/favicon.ico">
-        <title>Velocireno ${path !== "/" ? `| ${path.slice(1,2).toUpperCase()}${path.slice(2)}` : ""}</title>
-        <meta name="description" content="A simple, monolithic web application framework for Deno.">
+        <title>Velocireno ${request.url !== "/" ? `| ${request.url.slice(1,2).toUpperCase()}${request.url.slice(2)}` : ""}</title>
+        <meta name="description" content="The featherweight Deno + Preact webapp framework.">
         <meta name="keywords" content="site, description">
         <style>
             ${css}
