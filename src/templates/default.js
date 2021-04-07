8395e085be8f1f8c39e44d393672229c14b25315
Seb Ringrose <sebringrose@gmail.com>
2021-04-07 16:58:49 +0100

    compose & name corrections

--- src/templates/default.js
+++ src/templates/default.js
@@ -6,7 +6,7 @@ export default ({ request, css, script, html }) => `
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="/assets/favicon.ico">
-        <title>Velocireno ${request.url !== "/" ? `| ${request.url.slice(1,2).toUpperCase()}${request.url.slice(2)}` : ""}</title>
+        <title>Beko ${request.url !== "/" ? `| ${request.url.slice(1,2).toUpperCase()}${request.url.slice(2)}` : ""}</title>
         <meta name="description" content="The featherweight Deno + Preact webapp framework.">
         <meta name="keywords" content="site, description">
         <style>
