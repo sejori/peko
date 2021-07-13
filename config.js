ceef6a593d6ae984df205a0c225c26bb536c544f
Seb Ringrose <sebringrose@gmail.com>
2021-07-13 16:20:33 +0100

    updated path code for windows compatibility & moved html template to server-side utils

--- config.js
+++ config.js
@@ -20,9 +20,6 @@ export const reloadDelay = 400
 // location of client source files
 export const srcDir = `${Deno.cwd()}/src`
 
-// html template for ssr
-export const templateFile = `${Deno.cwd()}/src/templates/default.js`
-
 // globs for file searching in page ssr
 export const pagesGlob = `${Deno.cwd()}/src/pages/*.js`
 export const cssGlob = `${Deno.cwd()}/src/styles/*.css`
