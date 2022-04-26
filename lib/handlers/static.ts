328aeca09c6fcf5f587876df5021230395bfef90
Seb Ringrose <sebringrose@gmail.com>
2022-04-26 19:45:22 -0400

    use URL instead of path

--- lib/handlers/static.ts
+++ lib/handlers/static.ts
@@ -3,6 +3,8 @@ import { StaticRouteData } from "../types.ts"
 // I think there is a much more efficient method by streaming the file...
 export const staticHandler = async (_request: Request, staticData: StaticRouteData) => {
     let filePath = decodeURI(staticData.fileURL.pathname)
+
+    console.log(filePath)
     
     // fix annoying windows paths
     if (Deno.build.os === "windows") filePath = filePath.substring(1)
