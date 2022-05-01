f5b754d7c1c0f7b73c41c70ff0105983c5e7624b
Seb Ringrose <sebringrose@gmail.com>
2022-05-01 22:39:26 +0100

    WIP Vue example code added

--- examples/vue/src/app.js
+++ examples/vue/src/app.js
@@ -1,6 +1,6 @@
 import { createSSRApp } from "https://cdn.skypack.dev/vue@v3.2.23"
 
-export function createApp() {
+export default function createApp() {
   return createSSRApp({
     data: () => ({ count: 1 }),
     template: `<button @click="count++">{{ count }}</button>`
