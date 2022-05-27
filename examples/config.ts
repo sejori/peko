5cc3f09d7fd32aeadc8281e55dab5570a4bbe523
Sebastien Ringrose <seb@codeworks.me>
2022-05-27 13:53:48 +0100

    fix: configs

--- examples/config.ts
+++ examples/config.ts
@@ -6,45 +6,42 @@ const env = Deno.env.toObject()
 // Missing attributes fallback to default values (see lib/config.ts)
 //
 const config: Partial<Config> = {
-    // host set-up (same as default)
-    port: 7777,
-    hostname: "0.0.0.0",
+  // host set-up (same as default)
+  port: 7777,
+  hostname: "0.0.0.0",
 
-    // devMode true disables catching in addStaticRoute & addSSRRoute fcns
-    // TODO: implement hot-reloading SSE in devMode
-    devMode: env.ENVIRONMENT !== "production",
+  // devMode true disables catching in addStaticRoute & addSSRRoute fcns
+  // TODO: implement hot-reloading SSE in devMode
+  devMode: env.ENVIRONMENT !== "production",
 
-    // handle internally-generated log strings (same as default)
-    // Note: it is recommended to POST these to a logging service or db record
-    logString: (s) => console.log(s),
+  // handle internally-generated log strings (same as default)
+  // Note: it is recommended to POST these to a logging service or db record
+  logString: (s) => console.log(s),
 
-    // handle internally-generated event objects
-    // Note: it is recommended to POST these to an analytics service or db record
-    //
-    // Use commented default value below to see event data in console:
-    // logEvent: (data) => console.log(JSON.stringify(data)),
-    logEvent: (e) => console.log(e),
+  // handle internally-generated event objects
+  // Note: it is recommended to POST these to an analytics service or db record
+  logEvent: (e) => console.log(e),
 
-    // custom error handling function
-    // Note: must return a valid Response object
-    errorHandler: (statusCode, _request, _error) => {
-        let response;
-        switch (statusCode) {
-            case 404: 
-                response = new Response("404: Nothing found here!", {
-                    headers: new Headers(),
-                    status: 404
-                })
-                break
-            default:
-                response = new Response("500: Internal Server Error!", {
-                    headers: new Headers(),
-                    status: 500
-                })
-                break
-        }
-        return response
+  // custom error handling function
+  // Note: must return a valid Response object
+  errorHandler: (statusCode, _request, _error) => {
+    let response;
+    switch (statusCode) {
+      case 404: 
+        response = new Response("404: Nothing found here!", {
+          headers: new Headers(),
+          status: 404
+        })
+        break
+      default:
+        response = new Response("500: Internal Server Error!", {
+          headers: new Headers(),
+          status: 500
+        })
+        break
     }
+    return response
+  }
 }
 
 export default config
--- lib/config.ts
+++ lib/config.ts
@@ -1,11 +1,11 @@
 import { Config, Event } from "./types.ts"
 
 let config: Config = {
-  devMode: true,
+  devMode: false,
   port: 7777,
   hostname: "0.0.0.0",
   logString: (log: string) => console.log(log),
-  logEvent: (e: Event) => console.log(JSON.stringify(e)),
+  logEvent: (e: Event) => console.log(e),
   errorHandler: (statusCode: number) => {
     let response;
     switch (statusCode) {
