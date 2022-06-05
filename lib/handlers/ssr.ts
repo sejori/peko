53deae9963a066c5077ed9a80d5c051a1013ff9d
Seb Ringrose <sebringrose@gmail.com>
2022-06-05 11:49:31 +0100

    FIX: refactor custom route adders into handler files

--- lib/handlers/ssr.ts
+++ lib/handlers/ssr.ts
@@ -1,3 +1,6 @@
+import { addRoute } from "../server.ts"
+import { getConfig } from "../config.ts"
+import { createResponseCache } from "../utils/cacher.ts"
 import { HandlerParams, SSRRoute } from "../types.ts"
 import { hasher } from "../utils/hasher.ts"
 
@@ -42,5 +45,41 @@ export const ssrHandler = async (ssrData: SSRRoute, request: Request, params: Ha
       // not devMode + memoized: ETag matches "if-none-match" header so 304 (not modified) response given
       'ETag': hashString
     })
+  })
+}
+
+/**
+ * Add a route that uses the ssr handler and Response cache.
+ * 
+ * See "lib/types.ts" for Template, Render & CustomTags type details
+ * 
+ * @param ssrRouteData { 
+    route: string - e.g. "/home"
+    middleware?: Middleware (optional)
+    template: Template - e.g. (ssrResult, customTags, request) => `<!DOCTYPE html><html><head>${customTags.title}</head><body>${ssrResult}</body></html>`
+    render: Render - e.g. (app) => renderToString(app())
+    moduleURL: URL - e.g. new URL("./src/home.js")
+    customTags?: CustomTags - e.g. () => ({ title: <title>Home Page!</title> })
+    cacheLifetime?: number - e.g. 3600000
+ * }
+*/
+export const addSSRRoute = (ssrRouteData: SSRRoute) => {
+  const config = getConfig()
+
+  const memoizeHandler = createResponseCache({
+    lifetime: ssrRouteData.cacheLifetime
+  }) 
+
+  const cachedSSRHandler = memoizeHandler((request, params) => ssrHandler(ssrRouteData, request, params))
+
+  return addRoute({
+    route: ssrRouteData.route,
+    method: "GET",
+    middleware: ssrRouteData.middleware,
+    handler: async (request, params) => !config.devMode
+      // use cache-enabled fcn if not in prod env and pass in params 
+      // so we cache renders by params as well as SSRRoute data
+      ? await cachedSSRHandler(request, params)
+      : await ssrHandler(ssrRouteData, request, params)
   })
 }
