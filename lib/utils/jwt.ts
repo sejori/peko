6f717023f9ee5310a4c0ec863447dd80bd29c664
Sebastien Ringrose <seb@codeworks.me>
2022-06-22 16:49:54 +0100

    auth middleware setup complete

--- lib/utils/jwt.ts
+++ lib/utils/jwt.ts
@@ -1,36 +1,51 @@
 import { crypto } from "https://deno.land/std@0.144.0/crypto/mod.ts"
 import { logError } from "./logger.ts"
 
-const KEY = await crypto.subtle.generateKey({ name: "BLAKE3" }, true, ["sign", "verify"])
-// JWT implementation
+const KEY = await crypto.subtle.generateKey({
+  name: "HMAC",
+  hash: {name: "SHA-512"}
+}, true, ["sign", "verify"])
 
-export const decodeJWT = (jwt: string) => {
+const encoder = new TextEncoder()
+const decoder = new TextDecoder()
+
+export const decodeJWT = async (jwt: string) => {
   let [ header, payload, signature ] = jwt.split(".")
 
   try {
     header = JSON.parse(atob(header))
     payload = JSON.parse(atob(payload))
+    signature = decodeURIComponent(atob(signature))
   } catch(error) {
     logError("PARSE-JWT", error, new Date())
     return undefined
   }
 
-  const signatureInput = Array.from(`${header}.${payload}`).map(v => Number(v))
-  if (!crypto.subtle.verify({ name: "BLAKE3" }, KEY, signature, signatureInput)) return undefined
+  const signatureInput = btoa(`${header}.${payload}`)
+  const valid = await crypto.subtle.verify({
+    name: "HMAC",
+    hash: {name: "SHA-512"}
+  }, KEY, encoder.encode(signature), encoder.encode(signatureInput))
+  if (!valid) return undefined
 
   return payload
 }
 
-export const generateJWT = (sessionData: Record<string, unknown>) => {
+export const generateJWT = async (sessionData: Record<string, unknown>) => {
   const header = btoa(JSON.stringify({
-    alg: "BLAKE3",
+    alg: "HMAC",
     typ: "JWT"
   }))
 
   const payload = btoa(JSON.stringify(sessionData))
 
-  const signatureInput = Array.from(`${header}.${payload}`).map(v => Number(v))
-  const signature = crypto.subtle.sign({ name: "BLAKE3" }, KEY, signatureInput)
+  const signatureInput = `${header}.${payload}`
+  const signatureBuffer = await crypto.subtle.sign({
+    name: "HMAC",
+    hash: {name: "SHA-512"}
+  }, KEY, encoder.encode(signatureInput))
+
+  const signature = btoa(encodeURIComponent(decoder.decode(signatureBuffer)))
 
   return `${header}.${payload}.${signature}`
 }
