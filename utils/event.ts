91d5db304dcc8c67244dac5ea05787ffb41a529e
Sebastien Ringrose <seb@codeworks.me>
2022-07-12 22:46:47 +0100

    feat: emitter class refactor

--- utils/event.ts
+++ utils/event.ts
@@ -1,43 +1,38 @@
 import { Event } from "../server.ts"
 
-const emitters: Emitter[] = []
-
-export type Emitter = {
-  emit: (e: Event) => Promise<unknown[]> | undefined
-  subscribe: (cb: Listener) => boolean
-  unsubscribe: (cb: Listener) => boolean
-  listeners: Listener[]
-}
-
 export type Listener = (e: Event) => unknown | Promise<unknown>
 
 /**
- * Event emitter, designed to be used with the sseHandler
+ * Event emitter class, designed to be used with the sseHandler
  * @param initListeners: Listener[]
  * @returns emitter: Emitter
  */
-export const createEmitter = (initListeners?: Listener[] | Listener) => {
-  const listeners: Listener[] = initListeners && initListeners instanceof Array
-    ? initListeners 
-    : initListeners ? [initListeners] : []
+export class Emitter {
+  listeners: Listener[] = []
 
-  const subscribe = (listener: Listener) => {
-    listeners.push(listener)
+  constructor(initListeners?: Listener[] | Listener) {
+    this.listeners = initListeners instanceof Array
+      ? initListeners 
+      : initListeners ? [initListeners] : []
+  }
+
+  subscribe(listener: Listener) {
+    this.listeners.push(listener)
     return true
   }
 
-  const unsubscribe = (listener: Listener) => {
+  unsubscribe(listener: Listener) {
     // convert listener function bodies to strings for comparison
-    const emListener = listeners.find(li => li.toString() === listener.toString())
+    const emListener = this.listeners.find(li => li.toString() === listener.toString())
     if (emListener) {
-      listeners.splice(listeners.indexOf(emListener), 1)
+      this.listeners.splice(this.listeners.indexOf(emListener), 1)
       return true
     }
     return false
   }
 
   // async so won't block process when called without "await"
-  const emit = async (data: Record<string, unknown>) => {
+  async emit (data: Record<string, unknown>) {
     const date = new Date()
     const eventId = `EMIT-${JSON.stringify(data)}`
     const event: Event = { 
@@ -48,14 +43,9 @@ export const createEmitter = (initListeners?: Listener[] | Listener) => {
     }
     
     try {
-      return await Promise.all(listeners.map(async listener => await listener(event)))
+      return await Promise.all(this.listeners.map(async listener => await listener(event)))
     } catch (error) {
       throw(error)
     }
   }
-
-  const emitter = { emit, listeners, subscribe, unsubscribe }
-
-  emitters.push(emitter)
-  return emitter
 }
