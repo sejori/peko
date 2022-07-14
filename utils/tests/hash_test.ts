c991d2c599ac53428b926f58aef70f6a6cc13e7b
Seb Ringrose <sebringrose@gmail.com>
2022-07-14 16:14:01 +0100

    feat: event & hash tests

--- utils/tests/hash_test.ts
+++ utils/tests/hash_test.ts
@@ -1,2 +1,13 @@
-// HASH
-// test creates repeating hash
+import { assert } from "https://deno.land/std@0.147.0/testing/asserts.ts"
+import { hasher } from "../hash.ts"
+
+Deno.test("UTIL: HASH", async (t) => {
+  const str = "test-string-1234567890"
+  
+  await t.step("test creates repeating hash", async () => {
+    const hash1 = await hasher(str)
+    const hash2 = await hasher(str)
+
+    assert(hash1 === hash2)
+  })
+})
