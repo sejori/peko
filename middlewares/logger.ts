baf16db78a758e3e4549e273a905386fcd9ba1e5
Seb Ringrose <sebringrose@gmail.com>
2022-07-23 08:59:05 +0100

    feat: initial cache middleware

--- middlewares/logger.ts
+++ middlewares/logger.ts
@@ -1,11 +1,11 @@
-import { RequestContext } from "../server.ts"
+import { MiddlewareResult, RequestContext } from "../server.ts"
 
 /**
- * Logging middleware, uses cascading middleware to call PekoServer.logRequest after request is handled
+ * Logging middleware, awaits next() so logRequest happens after request is handled
  * @param ctx: RequestContext
  * @param next: () => MiddlewareResult (for cascading middleware)
  */
-export const logger = async (ctx: RequestContext, next: () => Promise<Response>) => {
+export const logger = async (ctx: RequestContext, next: () => Promise<Response>): Promise<MiddlewareResult> => {
   const start = Date.now()
   const response = await next()
   ctx.server.logRequest(ctx, response, start, Date.now() - start)
