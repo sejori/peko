5a85b3fad62739c262f4ae3ba840c8e0453f3b2c
Sebastien Ringrose <seb@codeworks.me>
2022-07-27 11:52:55 +0100

    fix: auth expiry check

--- middleware/authenticator.ts
+++ middleware/authenticator.ts
@@ -3,11 +3,11 @@ import { Crypto } from "../utils/Crypto.ts"
 
 const crypto = new Crypto({ key: "SUPER_SECRET_KEY_123" }) // <-- should come from env
 
-export interface Payload {
-  iat: number
-  exp: number
-  data: Record<string, string>
-}
+// export interface Payload {
+//   iat: number
+//   exp: number
+//   data: Record<string, string>
+// }
 
 /**
  * Auth middleware, uses Crypto utility class for JWTs with expiry
@@ -17,14 +17,18 @@ export interface Payload {
 export const authenticator = async (ctx: RequestContext): Promise<Response | void>=> {
   const authHeader = ctx.request.headers.get("Authorization")
 
+  // check JWT from cookies
   if (authHeader && authHeader.slice(0,7) === "Bearer ") {
     const jwt = authHeader.slice(7)
-    const payload: Payload | undefined = await crypto.verify(jwt)
-    if (payload && (!payload.exp || payload.exp > Date.now())) {
-      ctx.state.authPayload = payload
+    const payload = await crypto.verify(jwt)
+    if (typeof payload?.exp === "number" && payload.exp > Date.now()) {
+      ctx.state.auth = payload
       return
     }
   }
+
+  // otherwise create anonymous session
+  
   
   return await ctx.server.handleError(ctx, 401)
 }
