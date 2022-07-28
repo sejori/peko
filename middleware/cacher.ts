8e5b8cd1cdf6b7efe7884679bba090238cb9dc53
Seb Ringrose <sebringrose@gmail.com>
2022-07-28 19:17:06 +0100

    fix: pass instance of necessary util to middleware

--- middleware/cacher.ts
+++ middleware/cacher.ts
@@ -1,13 +1,10 @@
-import { RequestContext } from "../server.ts"
+import { RequestContext, Middleware } from "../server.ts"
 import { ResponseCache } from "../utils/ResponseCache.ts";
-
-const cache = new ResponseCache()
-
 /**
- * Caching middleware, uses ResponseCache utility
- * @param ctx: RequestContext
- * @returns MiddlewareResponse
+ * Generates a caching middleware using provided ResponseCache instance
+ * @param cache: ResponseCache
+ * @returns Middleware
  */
-export const cacher = (ctx: RequestContext, next: () => Promise<Response>): Promise<Response> => {
+export const cacher = (cache: ResponseCache): Middleware => (ctx: RequestContext, next: () => Promise<Response>): Promise<Response> => {
   return cache.memoize(next)(ctx)
 }
