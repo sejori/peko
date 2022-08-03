80712ee612e756207f84b6ef2d33d0b4c2cd8ef9
Seb Ringrose <sebringrose@gmail.com>
2022-08-03 16:48:09 +0100

    feat: cacher and logger tests

--- tests/middleware/cacher_test.ts
+++ tests/middleware/cacher_test.ts
@@ -1,30 +1,43 @@
 import { assert } from "https://deno.land/std@0.150.0/testing/asserts.ts"
 import { Server, RequestContext } from "../../server.ts"
-import { sseHandler } from "../../handlers/sse.ts"
-import { Emitter } from "../../utils/Emitter.ts"
+import { cacher } from "../../middleware/cacher.ts"
+import { ResponseCache } from "../../utils/ResponseCache.ts"
 
 Deno.test("MIDDLEWARE: Cacher", async (t) => {
+  const successString = "Success!"
+  const CACHE_LIFETIME = 100
+  const cache = new ResponseCache({
+    lifetime: CACHE_LIFETIME
+  })
   const server = new Server({
     eventLogger: () => {},
     stringLogger: () => {}
   })
-  const ctx = new RequestContext(server)
-  const emitter = new Emitter()
-  const decoder = new TextDecoder()
+
   const testData = {
     foo: "bar"
   }
   
-  await t.step("Response created and event data emitted as expected", async () => {
-    const response = await sseHandler(emitter)(ctx)
-    const reader = response.body?.getReader()
+  await t.step("Response created and cached as expected", async () => {
+    const ctx = new RequestContext(server, undefined, { ...testData })
+    const response1 = await cacher(cache)(ctx, async () => await new Response(successString))
+    assert(!ctx.state.responseFromCache && ctx.state.foo === testData.foo)
 
-    await emitter.emit(testData)
+    const response2 = await cacher(cache)(ctx, async () => await new Response(successString))
+    assert(ctx.state.responseFromCache === true && ctx.state.foo === testData.foo)
+    
+    const body1 = await response1?.text()
+    const body2 = await response2?.text()
+    assert(body1 === successString && body2 === successString)
+  }) 
 
-    if (reader) {
-      const { done, value } = await reader?.read()
-      assert(!done)
-      assert(decoder.decode(value) === `data: ${JSON.stringify(testData)}\n\n`)
-    }
+  await t.step("Cached response invalidated as expected", async () => {
+    await new Promise(res => setTimeout(res, CACHE_LIFETIME))
+    const ctx = new RequestContext(server, undefined, { ...testData })
+    const response = await cacher(cache)(ctx, async () => await new Response(successString))
+    const body = await response?.text()
+
+    assert(!ctx.state.responseFromCache && ctx.state.foo === testData.foo)
+    assert(body === successString)
   }) 
 })
