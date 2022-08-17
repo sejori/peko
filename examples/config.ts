383dc8bb1599a4e5c78f7f7bb417b2457450f757
Sebastien Ringrose <seb@codeworks.me>
2022-08-17 11:02:55 +0100

    fix: disable cache clean as Deno.memoryUsage is not supported on Deploy

--- examples/config.ts
+++ examples/config.ts
@@ -4,7 +4,7 @@ const env = Deno.env.toObject()
 
 const config = {
   // set devMode true to disable browser & server caching
-  devMode: env.ENVIRONMENT !== "production",
+  devMode: env.ENVIRONMENT === "production" ? true : false,
   globalMiddleware: [
     logger
   ],
