94278eec963b914c24260418438ac4d7c7e3ec3f
Sebastien Ringrose <seb@codeworks.me>
2022-08-17 16:50:33 +0100

    experiment: config

--- examples/config.ts
+++ examples/config.ts
@@ -2,13 +2,15 @@ import { logger } from "../mod.ts"
 
 const env = Deno.env.toObject()
 
+console.log(env)
+
 const config = {
   // set devMode true to disable browser & server caching
   devMode: env.ENVIRONMENT === "production" ? false : true,
   globalMiddleware: [
     logger
   ],
-  // eventLogger: () => {}, // <-- ingore event logs for clean shell
+  eventLogger: () => {}, // <-- ingore event logs for clean shell
 }
 
 export default config
