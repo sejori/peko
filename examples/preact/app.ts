4c5db75121c02f53b3378459911328a3074b5c07
Sebastien Ringrose <seb@codeworks.me>
2022-09-13 11:33:47 +0100

    feat: deno tasks in examples

--- examples/preact/app.ts
+++ examples/preact/app.ts
@@ -1,7 +1,8 @@
-import Server from "../../mod.ts" // <- https://deno.land/x/peko/server.ts 
-
+import Server from "../../server.ts" // <- https://deno.land/x/peko/server.ts 
 import config from "../config.ts"
-import { pages, assets, APIs } from "./routes.ts"
+import pages from "./routes/page.ts"
+import assets from "./routes/asset.ts"
+import APIs from "./routes/api.ts"
 
 // initialize server with config
 const server = new Server(config)
@@ -15,5 +16,5 @@ assets.forEach(asset => server.addRoute(asset))
 // Custom API functions
 APIs.forEach(API => server.addRoute(API))
 
-// Start Peko server :)
+// Start Peko server ^^
 server.listen()
