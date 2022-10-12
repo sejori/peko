ba6bbb3dbd32ec3a16d3966b7c37cd333e2b91b6
Seb Ringrose <sebringrose@gmail.com>
2022-10-12 22:42:02 +0100

    feat: circular ref class serialization @

--- tests/utils/Storage_test.ts
+++ tests/utils/Storage_test.ts
@@ -1,7 +1,7 @@
 import { assert } from "https://deno.land/std@0.150.0/testing/asserts.ts"
 import { Store, Model } from "../../utils/Storage.ts"
 
-Deno.test("UTIL: Store", async (t) => {
+Deno.test("UTIL: Storage", async (t) => {
   const store = new Store()
 
   class Person extends Model {
@@ -38,26 +38,17 @@ Deno.test("UTIL: Store", async (t) => {
   const fencing = new Hobby("fencing")
   const bob = new Person("Bob")
   const jim = new Person("Jim")
-
   bob.addFriend(jim)
 
-  await t.step("provided methods returned after serialization", () => {
+  await t.step("circular ref object retains methods after serializing and deserializing", () => {
     assert(bob.name === "Bob" && bob.friends.includes(jim))
 
-    try {
-      const jimId = jim.save()
-      const freshJim = store.load(jimId, Person)
-      console.log(freshJim)
+    const jimId = jim.save()
+    const freshJim = store.load(jimId) as Person
+    freshJim.addHobby(fencing)
 
-      freshJim.addHobby(fencing)
-    } catch(e) {
-      console.log(e)
-    }
-
-
-    // freshJim.addHobby(fencing)
-
-    // assert(freshJim.hobbies[0].getTitle() === "fencing")
-    // assert(freshJim.friends[0].name === "Bob")
+    assert(jim !== freshJim)
+    assert(freshJim.hobbies[0].getTitle() === "fencing")
+    assert(freshJim.friends[0].name === "Bob")
   }) 
 })
