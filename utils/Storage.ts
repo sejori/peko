3a477eac5c2d3043c6fdcd96f3bb2a5e10f1bf51
Sebastien Ringrose <seb@codeworks.me>
2022-10-12 20:05:48 +0100

    fix: store parsed list to stop endless load in Store.load method

--- utils/Storage.ts
+++ utils/Storage.ts
@@ -1,5 +1,5 @@
 /**
- * Generic data model class with serialization methods for storage.
+ * Store class with serialization methods for persistent storage.
  */
 
 export class Store {
@@ -7,10 +7,10 @@ export class Store {
   constructors: Map<string, () => Model> = new Map()
   items: Map<string, string> = new Map()
 
-  // constructor() {
-  //   this.constructors.set("Array", () => [] as unknown as Model)
-  //   this.constructors.set("Function", () => function () {} as unknown as Model)
-  // }
+  constructor() {
+    this.constructors.set("Array", () => [] as unknown as Model)
+    this.constructors.set("Function", () => function () {} as unknown as Model)
+  }
 
   save(node: Model) {
     // DFS - assign unique id to nodes for breadcrumbs
@@ -26,12 +26,11 @@ export class Store {
         }
       }
 
-      console.log(this);
-      this.items.set(node[this.code], JSON.stringify(node))
-      // , (_, value) => {
-      //   if (typeof value === "function") return `fcn+${value.toString()}`
-      // }))
+      const serialized = typeof node !== "function"
+        ? JSON.stringify(node)
+        : (node as Function).toString()
 
+      this.items.set(node[this.code], serialized)
       return node[this.code]
     }
 
@@ -39,6 +38,8 @@ export class Store {
   }
 
   load(rootId: string) {
+    const parsed: Map<string, Model> = new Map()
+
     const parseNode = (nodeId: string) => {
       const objType = nodeId.slice(0, nodeId.indexOf("+"))
       const constructor = this.constructors.get(objType)
@@ -47,17 +48,23 @@ export class Store {
       const nodeString = this.items.get(nodeId)
       if (!constructor) throw new Error(`No node string found for item with id ${nodeId}`)
 
-      console.log(nodeString)
-      const nodeObj = { ...constructor(), ...JSON.parse((nodeString as string)) }
+      const deserialized = objType === "Function"
+        ? new Function()
+        : JSON.parse((nodeString as string))
+
+      const nodeObj = { ...constructor(), ...deserialized }
+
+      parsed.set(nodeObj[this.code], nodeObj)
+      delete nodeObj[this.code]
 
       for (const key in nodeObj) {
-        console.log(nodeObj[key])
-        if (this.items.has(nodeObj[key])) {
+        if (parsed.has(nodeObj[key])) {
+          nodeObj[key] = parsed.get(nodeObj[key])
+        } else if (this.items.has(nodeObj[key])) {
           nodeObj[key] = parseNode(nodeObj[key])
         }
       }
 
-      delete nodeObj[this.code]
       return nodeObj
     }
 
