b3a6388d8414b3acbc48ffcf2926cc3cdee1820e
Sebastien Ringrose <seb@codeworks.me>
2022-10-12 22:42:02 +0100

    feat: circular ref class serialization @

--- utils/Storage.ts
+++ utils/Storage.ts
@@ -4,41 +4,37 @@
 
 export class Store {
   code = crypto.randomUUID()
-  constructors: Map<string, () => Model> = new Map()
-  items: Map<string, string> = new Map()
+  constructors = new Map()
+  items = new Map()
 
   constructor() {
-    this.constructors.set("Array", () => [] as unknown as Model)
-    this.constructors.set("Function", () => function () {} as unknown as Model)
+    this.constructors.set("Array", () => [])
+    this.constructors.set("Function", () => function () {})
   }
 
-  save(node: Model) {
+  save(node: any) {
     // DFS - assign unique id to nodes for breadcrumbs
     // then replace refs with linked node id and stringify to items
-    const storeNode = (node: Model) => {
-      const proto = Object.getPrototypeOf(node)
-      node[this.code] = `${proto.constructor.name}+${crypto.randomUUID()}`
+    const proto = Object.getPrototypeOf(node)
+    node[this.code] = `${proto.constructor.name}+${crypto.randomUUID()}`
 
-      for (const key in node) {
-        if (key !== "_storage" && node[key] === Object(node[key])) { // non-primitive
-          if (!node[key][this.code]) storeNode(node[key])
-          node[key] = node[key][this.code]
-        }
+    for (const key in node) {
+      if (node[key] === Object(node[key])) { // non-primitive
+        if (!node[key][this.code]) this.save(node[key])
+        node[key] = node[key][this.code]
       }
-
-      const serialized = typeof node !== "function"
-        ? JSON.stringify(node)
-        : (node as Function).toString()
-
-      this.items.set(node[this.code], serialized)
-      return node[this.code]
     }
 
-    return storeNode(node)
+    const serialized = typeof node !== "function"
+      ? JSON.stringify(node)
+      : node.toString() // TODO: check this
+
+    this.items.set(node[this.code], serialized)
+    return node[this.code]
   }
 
-  load<T>(rootId: string, _type: T): T {
-    const parsed: Map<string, Model> = new Map()
+  load(rootId: string) {
+    const parsed = new Map()
 
     const parseNode = (nodeId: string) => {
       const objType = nodeId.slice(0, nodeId.indexOf("+"))
@@ -46,14 +42,14 @@ export class Store {
       if (!constructor) throw new Error(`No constructor found for ${objType} in id ${nodeId}`)
       
       const nodeString = this.items.get(nodeId)
-      if (!constructor) throw new Error(`No node string found for item with id ${nodeId}`)
+      if (!nodeString) throw new Error(`No node string found for item with id ${nodeId}`)
 
-      const deserialized = objType === "Function"
-        ? new Function()
-        : JSON.parse((nodeString as string))
+      const deserialized = objType !== "Function"
+        ? JSON.parse(nodeString)
+        : new Function()
 
-      const baseObj = constructor()
-      const nodeObj = { ...baseObj, ...deserialized }
+      const nodeObj = constructor()
+      Object.assign(nodeObj, deserialized)
 
       parsed.set(nodeObj[this.code], nodeObj)
       delete nodeObj[this.code]
@@ -69,24 +65,21 @@ export class Store {
       return nodeObj
     }
 
-    return parseNode(rootId) as T
+    return parseNode(rootId)
   }
 }
 
 export class Model {
-  [index: string | number]: any;
-  _storage: Store
-  _constructor: () => this
+  #storage
 
   constructor(store: Store, constructArgs: IArguments) {
     const This = Object.getPrototypeOf(this).constructor
 
-    this._storage = store
-    this._constructor = () => new This(...constructArgs)
-    this._storage.constructors.set(This.name, this._constructor)
+    this.#storage = store
+    this.#storage.constructors.set(This.name, () => new This(...constructArgs))
   }
 
   save() {
-    return this._storage.save(this)
+    return this.#storage.save(this)
   }
 }
