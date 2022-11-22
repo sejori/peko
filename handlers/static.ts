735b55383881d045554f86905a9de183165b3417
Sebastien Ringrose <seb@codeworks.me>
2022-11-22 09:28:40 +0000

    feat: optional body transform in static handler

--- handlers/static.ts
+++ handlers/static.ts
@@ -4,21 +4,24 @@ import { Crypto } from "../utils/Crypto.ts"
 import { mergeHeaders } from "../utils/helpers.ts";
 
 const crypto = new Crypto(Array.from({length: 10}, () => Math.floor(Math.random() * 9)).toString())
+export interface staticHandlerOptions extends HandlerOptions {
+  transform?: (contents: Uint8Array) => BodyInit
+}
 
 /**
- * Generates Response with body from file URL, sets modifiable
- * "Cache-Control" header and hashes file contents for "ETAG" header.
+ * Generates Response body from file URL and Content-Type and ETAG headers.
+ * Optionally: provide custom headers and/or body transform fcn.
+ * 
  * @param fileURL: URL object of file address
  * @param opts: (optional) HandlerOptions
  * @returns Handler: (ctx: RequestContext) => Promise<Response>
  */
-export const staticHandler = (fileURL: URL, opts: HandlerOptions = {}): Handler => async (_ctx: RequestContext) => {
+export const staticHandler = (fileURL: URL, opts: staticHandlerOptions = {}): Handler => async (_ctx: RequestContext) => {
   let filePath = decodeURI(fileURL.pathname)
   
   // fix annoying windows paths
   if (Deno.build.os === "windows") filePath = filePath.substring(1)
 
-  // Is it more efficient to stream file into response body?
   const body = await Deno.readFile(filePath)
   const hashString = await crypto.hash(body.toString())
   const ctHeader = contentType(filePath.slice(filePath.lastIndexOf(".")))
@@ -27,7 +30,9 @@ export const staticHandler = (fileURL: URL, opts: HandlerOptions = {}): Handler 
     "ETag": hashString,
     "Content-Type": ctHeader ? ctHeader : "text/plain"
   })
+
   if (opts.headers) mergeHeaders(headers, opts.headers)
+  if (opts.transform) return new Response(opts.transform(body), { headers })
 
   return new Response(body, { headers })
 }
