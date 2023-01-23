3df09a0bc05bc2dac14c0b9088dbb75a78288ce0
Sebastien Ringrose <seb@codeworks.me>
2023-01-23 16:56:40 +0000

    feat: crypto rework

--- utils/Crypto.ts
+++ utils/Crypto.ts
@@ -1,13 +1,6 @@
 import { crypto } from "https://deno.land/std@0.150.0/crypto/mod.ts"
-import { DigestAlgorithm } from "https://deno.land/std@0.150.0/_wasm_crypto/mod.ts";
-import { encode as encodeB64, decode as decodeB64 } from "https://deno.land/std@0.150.0/encoding/base64url.ts";
+import { encode as encodeB64, decode as decodeB64 } from "https://deno.land/std@0.150.0/encoding/base64url.ts"
 const encoder = new TextEncoder()
-
-type Payload = {
-  exp: number,
-  iat: number,
-  data: Record<string, unknown>
-}
 
 /**
  * Crypto class, generates hashes and signs and verifies JWTs using provided key.
@@ -15,23 +8,24 @@ type Payload = {
  */
 export class Crypto {
   key: CryptoKey | undefined
-  rawKey: string | undefined
-  algorithm: { name: string, hash: DigestAlgorithm } = { name: "HMAC", hash: "SHA-256" }
 
   constructor(key: CryptoKey | string) {
-    if (typeof key === "string") {
-      this.rawKey = key
-    } else this.key = key
+    if (key instanceof CryptoKey) this.key = key
+    else Crypto.createCryptoKey(key)
+      .then(result => this.key = result)
   }
 
   /**
    * Create CryptoKey from rawKey string to be used in crypto methods
    */
-  async createCryptoKey(): Promise<void> {
-    this.key = await crypto.subtle.importKey(
+  static async createCryptoKey(rawKey: string): Promise<CryptoKey> {
+    console.log(rawKey)
+    console.log(encoder.encode(rawKey))
+    console.log(encoder.encode(rawKey).buffer.slice(0, 32))
+    return await crypto.subtle.importKey(
       "raw",
-      encoder.encode(await this.hash(this.rawKey as string)).buffer.slice(32),
-      this.algorithm,
+      encoder.encode(rawKey).buffer.slice(0, 32),
+      { name: "HMAC", hash: "SHA-256"},
       false, //extractable
       ["sign", "verify"]
     )
@@ -43,34 +37,34 @@ export class Crypto {
    * @returns hashHex: string
    */
   async hash(contents: string): Promise<string> {
-    const hashBuffer = await crypto.subtle.digest(
-      this.algorithm.hash,
-      encoder.encode(contents),
-    )
+    if (!this.key) throw new Error("No valid crypto key.")
+
+    // TODO: need to work out why crypto key length is being a bitch
+    // and need to write/find conversion utils between KeyAlgorithm and DigestAlgorithm
+    // also need to write/find conversion util for KeyAlgorithm to shorthand string as seen here:
+    // https://developers.google.com/identity/protocols/oauth2/service-account#httprest
+
+    const hashBuffer = await crypto.subtle.digest(this.key.algorithm, encoder.encode(contents))
     return encodeB64(hashBuffer)
   }
 
   /**
    * Sign (create) JWT from payload
-   * @param payload: {
-   *   exp: number,
-   *   iat: number,
-   *   data: Record<string, unknown>
-   * }
-   * @returns jwt: string
+   * @param payload: Record<string, unknown>
+   * @returns jwt: Promise<string>
    */
-  async sign (payload: Payload): Promise<string> {
-    if (!this.key) await this.createCryptoKey()
+  async sign (payload: Record<string, unknown>): Promise<string> {
+    if (!this.key) throw new Error("No valid crypto key.")
 
     const b64Header = encodeB64(JSON.stringify({
-      alg: this.algorithm,
+      alg: this.key.algorithm,
       typ: "JWT"
     }))
     const b64Payload = encodeB64(JSON.stringify(payload))
 
     const signatureBuffer = await crypto.subtle.sign(
-      this.algorithm, 
-      this.key as CryptoKey, 
+      this.key.algorithm, 
+      this.key, 
       encoder.encode(`${b64Header}.${b64Payload}`)
     )
     const signature = encodeB64(signatureBuffer)
@@ -81,32 +75,24 @@ export class Crypto {
   /**
    * Verify JWT and extract payload
    * @param jwt: string
-   * @returns payload: {
-   *   exp: number,
-   *   iat: number,
-   *   data: Record<string, unknown>
-   * }
+   * @returns payload: Promise<Record<string, unknown> | false>
    */
-  async verify (jwt: string): Promise<Payload | undefined> {
-    if (jwt.split(".").length != 3) return undefined
-    if (!this.key) await this.createCryptoKey()
-    
-    const [ b64Header, b64Payload, b64Signature ] = jwt.split(".")
+  async verify (jwt: string): Promise<Record<string, unknown> | false> {
+    if (!this.key) throw new Error("No valid crypto key.")
+
+    const split = jwt.split(".")
+    if (split.length != 3) return false
+    const [ b64Header, b64Payload, b64Signature ] = split
 
     const verified = await crypto.subtle.verify(
-      this.algorithm, 
-      this.key as CryptoKey, 
+      this.key.algorithm, 
+      this.key, 
       decodeB64(b64Signature),
       encoder.encode(`${b64Header}.${b64Payload}`)
     )
-    if (!verified) return undefined
-  
-    try {
-      const payload = JSON.parse(atob(b64Payload))
-      if (payload.exp < Date.now()) return undefined
-      return payload
-    } catch(error) {
-      throw(error)
-    }
+
+    return verified
+      ? JSON.parse(atob(b64Payload))
+      : false
   }
 }
