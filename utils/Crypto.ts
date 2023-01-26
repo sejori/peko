676eccc299dc0593d5fb249ef005744db49fb2eb
Seb Ringrose <sebringrose@gmail.com>
2023-01-26 17:47:20 +0000

    supporting raw HMAC keys

--- utils/Crypto.ts
+++ utils/Crypto.ts
@@ -2,27 +2,35 @@
 import { encode as encodeB64, decode as decodeB64 } from "https://deno.land/std@0.150.0/encoding/base64url.ts"
 const encoder = new TextEncoder()
 
+type AlgData = { name: "RSA-OAEP" | "HMAC", hash: "SHA-256" | "SHA-384" | "SHA-512" | "BLAKE3" }
+
 /**
  * Crypto class, generates hashes and signs and verifies JWTs using CyptoKey (generated from string or provided).
  * @param key: CryptoKey | string
  */
 export class Crypto {
-  key: CryptoKey | string
-
-  constructor(key: CryptoKey | string) {
-    this.key = key
-  }
+  constructor(public key: CryptoKey | JsonWebKey | string, public algData: AlgData) {}
 
   /**
    * Create CryptoKey from rawKey string to be used in crypto methods
    */
-  static async createCryptoKey(rawKey: string): Promise<CryptoKey> {
+  static async createCryptoKey(key: JsonWebKey | string, algData: AlgData): Promise<CryptoKey> {
+    if (typeof key === "string") {
+      if (algData.name !== "HMAC") throw new Error("Raw keys only supported for HMAC algorithm. Use jwk for RSA-OAEP.")
+      return await crypto.subtle.importKey(
+        "raw",
+        encoder.encode(key),
+        algData,
+        false, //extractable, let user define this?
+        ["verify", "sign"]
+      )
+    }
     return await crypto.subtle.importKey(
-      "raw",
-      encoder.encode(rawKey),
-      { name: "HMAC", hash: "SHA-256"}, // only supported algorithm in Deno crypto API?
-      false, //extractable
-      ["sign", "verify"]
+      "jwk",
+      key,
+      algData,
+      false, //extractable, let user define this?
+      ["verify", "sign"]
     )
   }
 
@@ -32,10 +40,10 @@ export class Crypto {
    * @returns hashHex: string
    */
   async hash(contents: string): Promise<string> {
-    if (!(this.key instanceof CryptoKey)) this.key = await Crypto.createCryptoKey(this.key)
+    if (!(this.key instanceof CryptoKey)) this.key = await Crypto.createCryptoKey(this.key, this.algData)
 
     // TODO: may need to give users more fine-grained control of doigest algorithmm
-    const hashBuffer = await crypto.subtle.digest("SHA-256", encoder.encode(contents))
+    const hashBuffer = await crypto.subtle.digest(this.algData.hash, encoder.encode(contents))
     return encodeB64(hashBuffer)
   }
 
@@ -45,11 +53,10 @@ export class Crypto {
    * @returns jwt: Promise<string>
    */
   async sign (payload: Record<string, unknown>): Promise<string> {
-    if (!(this.key instanceof CryptoKey)) this.key = await Crypto.createCryptoKey(this.key)
-
-    // TODO: can I hard code this to RS256?
+    if (!(this.key instanceof CryptoKey)) this.key = await Crypto.createCryptoKey(this.key, this.algData)
+    
     const b64Header = encodeB64(JSON.stringify({
-      alg: "RS256",
+      alg: "RS256", // TODO: Need to deduce this from crypto key
       typ: "JWT"
     }))
     const b64Payload = encodeB64(JSON.stringify(payload))
@@ -70,7 +77,7 @@ export class Crypto {
    * @returns payload: Promise<Record<string, unknown> | false>
    */
   async verify (jwt: string): Promise<Record<string, unknown> | false> {
-    if (!(this.key instanceof CryptoKey)) this.key = await Crypto.createCryptoKey(this.key)
+    if (!(this.key instanceof CryptoKey)) this.key = await Crypto.createCryptoKey(this.key, this.algData)
 
     const split = jwt.split(".")
     if (split.length != 3) return false
