7c904f432c38c5980be2180d259d4ecff0bb7c35
Sebastien Ringrose <seb@codeworks.me>
2023-01-29 22:05:30 +0000

    feat: update docs

--- examples/preact/src/components/Layout.js
+++ examples/preact/src/components/Layout.js
@@ -1,19 +1,33 @@
 import { html } from "https://npm.reversehttp.com/preact,preact/hooks,htm/preact,preact-render-to-string"
 
-const Layout = ({ navColor, children }) => {
+const Layout = ({ navColor, navLink, children }) => {
   return html`
     <nav style=${navStyle(navColor)}>
-      <div class="container row">
-        <a style=${navLinkStyle} href="/">Home</a>
-        <img height="100px" width="100px" style="margin: 1rem;" src="/assets/twemoji_chick.svg" alt="peko-chick" />
-        <a style=${navLinkStyle} href="/about">About</a>
+      <div class="container align-center">
+        <img height="200px" width="200px" style="margin: 1rem;" src="/assets/DALL-Echu.webp" alt="peko-chick" />  
+        <h1 style="text-align: center;">Peko</h1>
+        <h2 style="text-align: center;"><i>Featherweight <a href="/${navLink}">apps</a> on Deno Deploy</i></h2>
       </div>
     </nav>
     <main style="padding: 1rem;" class="container">
       ${children}
     </main>
     <footer>
-      <p style="margin: 5px; text-align: center">Build for the open source community by <a style="color: white;" href="https://github.com/sebringrose">Seb Ringrose</a></p>
+      <div class="container row">
+        <a style=${navLinkStyle} href="https://github.com/sebringrose/peko">
+          <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" width=100 height=100 alt=GitHub/>
+          Source repo
+        </a>
+        <a class="align-center" style=${navLinkStyle} href="https://doc.deno.land/https://deno.land/x/peko/mod.ts">
+          <img src="https://raw.githubusercontent.com/denolib/high-res-deno-logo/master/deno_hr_circle.svg" width=100 height=100 alt=Deno />
+          API docs
+        </a>
+      </div>
+      <div class="container row">
+        <a style=${navLinkStyle} href="/">Home</a>
+        <a style=${navLinkStyle} href="/about">About</a>
+      </div>
+      <p style="margin: 10px; text-align: center">Built by <a style="color: white;" href="https://github.com/sebringrose">Seb Ringrose</a></p>
     </footer>
   `   
 }
@@ -23,16 +37,17 @@ const navStyle = (navColor) => `
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  color: white;
   background-color: ${navColor};
+  padding-bottom: 20px;
 `
 
 const navLinkStyle = `
   flex: 1;
-  text-align: center;
-  font-weight: bold;
-  font-size: 1.2rem;
-  margin: 1rem 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1rem;
   color: white;
   padding: 0px 5px;
 `
