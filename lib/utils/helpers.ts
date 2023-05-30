02943ee9a80dec5a128100060ecc6688091da46f
Sebastien Ringrose <seb@codeworks.me>
2023-05-30 11:36:26 +0100

    feat: routeGen fcn in routesFromDir

--- lib/utils/helpers.ts
+++ lib/utils/helpers.ts
@@ -20,31 +20,26 @@ export const mergeHeaders = (base: Headers, source: Headers) => {
  * Recursively create an array of routes with the static handler 
  * for all files/sub-directories in the provided directory.
  * @param dirUrl: URL
- * @param middleware: Middleware for each route added
- * @param routes: init Route array
+ * @param routeGen: (path: string, url: URL) => Route
  * @param _depth: used internally to correct paths, don't set
  * @returns routes: Route[]
  */
-export const routesFromDir = async (dirUrl: URL, handlerGen: (url: URL) => Handler, middleware?: Middleware | Middleware[], _depth = 1): Promise<Route[]> => {
+export const routesFromDir = async (dirUrl: URL, routeGen: (path: `/${string}`, url: URL) => Route, _depth = 1): Promise<Route[]> => {
   if (!(await Deno.stat(dirUrl)).isDirectory) throw new Error("URL does not point to directory.")
   const routes: Route[] = []
 
   for await (const file of Deno.readDir(dirUrl)) {
     const fileUrl = new URL(`file://${dirUrl.pathname}/${file.name}`)
     if (file.isDirectory) {
-      const subDirRoutes = await routesFromDir(fileUrl, handlerGen, middleware, _depth+1)
+      const subDirRoutes = await routesFromDir(fileUrl, routeGen, _depth+1)
       routes.splice(0, 0, ...subDirRoutes)
     } else {
-      const pieces = dirUrl.pathname.split("/")
-      let dirPath = ''
-      for (let i=1; i<_depth; i++) dirPath = `${pieces[pieces.length-i]}/${dirPath}`
+      const pieces = dirUrl.pathname.split("/").filter(Boolean)
+      let dirPath: `/${string}` = '/'
+      for (let i=1; i<=_depth; i++) dirPath = `/${pieces[pieces.length-i]}${dirPath}`
 
-      const filePath = `${dirPath}${file.name}`
-      routes.push({
-        path: `/${filePath}`,
-        middleware,
-        handler: handlerGen(fileUrl)
-      })
+      const filePath: `/${string}` = `${dirPath}${file.name}`
+      routes.push(routeGen(filePath, fileUrl))
     }
   }
 
