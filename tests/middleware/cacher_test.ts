d8a95a608dc5597107486d1d1b79c66925f22b68
Sebastien Ringrose <seb@codeworks.me>
2023-08-21 12:36:12 +0100

    feat: custom caching

--- tests/middleware/cacher_test.ts
+++ tests/middleware/cacher_test.ts
@@ -2,38 +2,64 @@ import { assert } from "https://deno.land/std@0.174.0/testing/asserts.ts"
 import { Router, RequestContext } from "../../lib/Router.ts"
 import { cacher } from "../../lib/middleware/cacher.ts"
 import { testHandler } from "../mocks/middleware.ts"
-import { ResponseCache } from "../../lib/utils/ResponseCache.ts"
+import { CacheItem, defaultKeyGen } from "../../lib/utils/CacheItem.ts"
 
 Deno.test("MIDDLEWARE: Cacher", async (t) => {
-  const server = new Router()
+  const router = new Router()
   const successString = "Success!"
-  const CACHE_LIFETIME = 100
-  const cache = new ResponseCache({
-    lifetime: CACHE_LIFETIME
-  })
-  const memHandler = cacher(cache)
-
   const testData = {
     foo: "bar"
   }
-  
-  await t.step("Response created and cached as expected", async () => {
-    const ctx = new RequestContext(server, new Request("http://localhost"), { ...testData })
-    const response1 = await cacher(cache)(ctx, () => new Response(successString))
-    assert(!ctx.state.responseFromCache && ctx.state.foo === testData.foo)
 
-    const response2 = await cacher(cache)(ctx, () => new Response(successString))
-    assert(ctx.state.responseFromCache === true && ctx.state.foo === testData.foo)
+  const defaultCacher = cacher()
+
+  const itemMap: Map<string, CacheItem> = new Map()
+  const CACHE_LIFETIME = 100
+  const customCacher = cacher({
+    itemLifetime: CACHE_LIFETIME,
+    keyGen: (ctx) => "I-make-key" + defaultKeyGen(ctx),
+    store: {
+      get: (key) => itemMap.get(key),
+      set: (key, value) => itemMap.set(key, value),
+      delete: (key) => itemMap.delete(key)
+    }
+  })
+  
+  await t.step("Responses cached as expected", async () => {
+    const ctx_default = new RequestContext(router, new Request("http://localhost"), { ...testData })
+    const ctx_custom = new RequestContext(router, new Request("http://localhost"), { ...testData })
+    const default_response1 = await defaultCacher(ctx_default, () => new Response(successString))
+    const custom_response1 = await customCacher(ctx_custom, () => new Response(successString))
+    console.log(ctx_default)
+    console.log(ctx_custom)
+    assert(!ctx_default.state.responseFromCache)
+    assert(ctx_default.state.foo === testData.foo)
+    assert(!ctx_custom.state.responseFromCache)
+    assert(ctx_custom.state.foo === testData.foo)
+
+    const default_response2 = await defaultCacher(ctx_default, () => new Response(successString))
+    const custom_response2 = await customCacher(ctx_custom, () => new Response(successString))
+    console.log(ctx_default)
+    console.log(ctx_custom)
+    assert(ctx_default.state.responseFromCache)
+    assert(ctx_default.state.foo === testData.foo)
+    assert(ctx_custom.state.responseFromCache)
+    assert(ctx_custom.state.foo === testData.foo)
     
-    const body1 = await response1?.text()
-    const body2 = await response2?.text()
-    assert(body1 === successString && body2 === successString)
+    const default_body1 = await default_response1?.text()
+    const default_body2 = await default_response2?.text()
+    assert(default_body1 === successString && default_body2 === successString)
+
+    const custom_body1 = await custom_response1?.text()
+    const custom_body2 = await custom_response2?.text()
+    assert(custom_body1 === successString)
+    assert(custom_body2 === successString)
   }) 
 
   await t.step("Cached response invalidated as expected", async () => {
     await new Promise(res => setTimeout(res, CACHE_LIFETIME))
-    const ctx = new RequestContext(server, new Request("http://localhost"), { ...testData })
-    const response = await cacher(cache)(ctx, () => new Response(successString))
+    const ctx = new RequestContext(router, new Request("http://localhost"), { ...testData })
+    const response = await customCacher(ctx, () => new Response(successString))
     const body = await response?.text()
 
     assert(!ctx.state.responseFromCache && ctx.state.foo === testData.foo)
@@ -41,9 +67,9 @@ Deno.test("MIDDLEWARE: Cacher", async (t) => {
   })
 
   await t.step("memoize - preserve handler logic", async () => {
-    const ctx = new RequestContext(server, new Request("http://localhost"), { thing: "different" })
+    const ctx = new RequestContext(router, new Request("http://localhost"), { thing: "different" })
     const testRes = await testHandler(ctx)
-    const memRes = await memHandler(ctx, () => testHandler(ctx))
+    const memRes = await customCacher(ctx, () => testHandler(ctx))
     assert(memRes)
 
     const testJSON = await testRes.json()
@@ -54,16 +80,14 @@ Deno.test("MIDDLEWARE: Cacher", async (t) => {
 
   await t.step("return 304 with matching ETAG", async () => {
     const ETag = "1234567890"
-    
-    // consider testing for correct key generation as logic is set to include path + params
-    const ctx = new RequestContext(server, new Request("http://localhost/static-content?test-thing=hello", {
+    const ctx = new RequestContext(router, new Request("http://localhost/static-content?test-thing=hello", {
       headers: new Headers({ "if-none-match": ETag })
     }))
 
-    const memRes = await memHandler(ctx, () => new Response("hello", {
+    const memRes = await customCacher(ctx, () => new Response("hello", {
       headers: new Headers({ "ETag": ETag })
     }))
-    const memRes2 = await memHandler(ctx, () => new Response("hello", {
+    const memRes2 = await customCacher(ctx, () => new Response("hello", {
       headers: new Headers({ "ETag": ETag })
     }))
 
