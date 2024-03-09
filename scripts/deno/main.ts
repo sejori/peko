ac56c57d3c7c799632461df98e8200d6b62ed60f
Sebastien Ringrose <seb.ringrose@pynea.com>
2024-03-09 15:33:19 +0000

    fix: ports in CI

--- scripts/deno/main.ts
+++ scripts/deno/main.ts
@@ -1,6 +1,11 @@
 import router from "../../example/preactSSR/router.ts";
 
 // Start Deno server with Peko router :^)
-Deno.serve((req) => router.handle(req));
+Deno.serve(
+  {
+    port: 7777,
+  },
+  (req) => router.handle(req)
+);
 
 console.log("Deno server running with Peko router <3");
