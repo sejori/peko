937ec3801be976883f421f31d7773b6f0dfbe88d
Seb Ringrose <sebringrose@gmail.com>
2024-06-02 07:21:23 +0100

    feat: schema typing nearly complete

--- tests/utils/Schema_test.ts
+++ tests/utils/Schema_test.ts
@@ -1,70 +1,68 @@
-import { Type, Schema } from "../../lib/utils/Schema.ts";
+import { Type, Schema, Int } from "../../lib/utils/Schema.ts";
 
-const user = new Type(
-  "User",
-  {
+const user = new Type({
+  name: "User",
+  fields: {
     id: String,
     userName: String,
     email: String,
     age: {
       type: Number,
-      resolver: (users) => [20],
       validator: (value) => value > 18,
+      res: "",
     },
   },
-  (ids) => [
+  resolver: async (ids) => [
     {
       id: "1",
       userName: "peko",
-      email: "test@peko.com",
-      age: 20,
     },
-  ]
-);
+  ],
+});
 
-const content = new Type(
-  "Content",
-  {
-    title: String,
-    content: String,
-  },
-  (ids) => [
-    {
-      title: "Hello",
-      content: "World",
-    },
-  ]
-);
+// const content = new Type(
+//   "Content",
+//   {
+//     title: String,
+//     content: String,
+//   },
+//   (ids) => [
+//     {
+//       title: "Hello",
+//       content: "World",
+//     },
+//   ]
+// );
 
-const post = new Type(
-  "Post",
-  {
-    author: {
-      type: user,
-      resolver: (posts) => [user],
-    },
-    content: {
-      type: user,
-      resolver: (posts) => [user],
-    },
-    status: {
-      type: String,
-      validator: (item: string) => ["draft", "published"].includes(item),
-    },
-  },
-  (ids) => [{}]
-);
+// const post = new Type(
+//   "Post",
+//   {
+//     author: {
+//       type: user,
+//       resolver: (posts) => [user],
+//     },
+//     content: {
+//       type: user,
+//       resolver: (posts) => [user],
+//     },
+//     status: {
+//       type: String,
+//       validator: (item: string) => ["draft", "published"].includes(item),
+//     },
+//   },
+//   (ids) => [{}]
+// );
 
-const comment = new Type(
-  "Comment",
-  {
-    author: {
-      type: user,
-      resolver: (comments) => [user],
-    },
-    text: String,
-  },
-  (ids) => [{}]
-);
+// const comment = new Type(
+//   "Comment",
+//   {
+//     author: {
+//       type: user,
+//       resolver: (comments) => [user],
+//     },
+//     text: String,
+//   },
+//   (ids) => [{}]
+// );
 
-const schema = new Schema([]);
+// const schema = new Schema([]);
