6a8939eab47ee4b96a1cc45baa4b7a557c9244d6
Seb Ringrose <sebringrose@gmail.com>
2024-06-25 16:22:36 +0100

    feat: init schema generation fixed

--- tests/utils/Schema_test.ts
+++ tests/utils/Schema_test.ts
@@ -1,74 +1,253 @@
-import { Type, DTO, Int } from "../../lib/utils/Schema.ts";
+import { assert } from "https://deno.land/std@0.218.0/assert/mod.ts";
+import {
+  Type,
+  Field,
+  Int,
+  Input,
+  Schema,
+  Mutation,
+  Query,
+} from "../../lib/utils/Schema.ts";
 
-const createUser = new DTO("userCreate", {
-  username: String,
-  age: {
-    type: Int,
-    validator: (x) => typeof x === "number" && x > 18,
-    resolver: async () => [19],
-  },
+Deno.test("UTIL: Profiler", async (t) => {
+  const email = new Field(String, {
+    validator: (x) => x.includes("@") && x.includes("."),
+  });
+
+  const age = new Field(Int, {
+    validator: (x) => typeof x === "number" && x > 0,
+  });
+
+  const userInput = new Input("CreateUser", {
+    fields: {
+      email,
+      age,
+    },
+  });
+
+  const user = new Type("User", {
+    fields: {
+      email,
+      age,
+    },
+    resolver: async (ctx) => [
+      {
+        email: "test@test.com",
+        age: 20,
+      },
+    ],
+  });
+
+  const registerUser = new Mutation("RegisterUser", {
+    input: userInput,
+    output: user,
+    resolver: async (ctx) => [
+      {
+        email: "test@test.com",
+        age: 20,
+      },
+    ],
+  });
+
+  const content = new Type("Content", {
+    fields: {
+      title: new Field(String),
+      content: new Field(String),
+    },
+    resolver: async (ctx) => [
+      {
+        title: "Hello",
+        content: "World",
+      },
+    ],
+  });
+
+  const createContent = new Mutation("CreateContent", {
+    input: new Input("CreateContent", {
+      fields: content.config.fields,
+    }),
+    output: content,
+    resolver: async (ctx) => [
+      {
+        title: "Hello",
+        content: "World",
+      },
+    ],
+  });
+
+  const post = new Type("Post", {
+    fields: {
+      author: new Field(user, {
+        resolver: async (posts) => [
+          {
+            email: "test@test.com",
+            age: 20,
+          },
+        ],
+      }),
+      content: new Field(content, {
+        resolver: async (posts) => [
+          {
+            title: "Hello",
+            content: "World",
+          },
+        ],
+      }),
+      status: new Field(String, {
+        validator: (item) => ["draft", "published"].includes(item.toString()),
+      }),
+    },
+    resolver: async (ids) => [
+      {
+        author: {
+          email: "test@test.com",
+          age: 20,
+        },
+        content: {
+          title: "Hello",
+          content: "World",
+        },
+        status: "published",
+      },
+    ],
+  });
+
+  const postContent = new Mutation("PostContent", {
+    input: new Input("PostContent", {
+      fields: post.config.fields,
+    }),
+    output: post,
+    resolver: async (ctx) => [
+      {
+        author: {
+          email: "test@test.com",
+          age: 20,
+        },
+        content: {
+          title: "Hello",
+          content: "World",
+        },
+        status: "published",
+      },
+    ],
+  });
+
+  const comment = new Type("Comment", {
+    fields: {
+      author: new Field(user, {
+        resolver: async (comments) => [
+          {
+            email: "test@test.com",
+            age: 20,
+          },
+        ],
+      }),
+      post: new Field(post, {
+        resolver: async (comments) => [
+          {
+            author: {
+              email: "test@test.com",
+              age: 20,
+            },
+            content: {
+              title: "Hello",
+              content: "World",
+            },
+            status: "published",
+          },
+        ],
+      }),
+      text: new Field(String),
+    },
+    resolver: async (ids) => [
+      {
+        author: {
+          email: "",
+          age: 20,
+        },
+        post: {
+          author: {
+            email: "test@test.com",
+            age: 20,
+          },
+          content: {
+            title: "Hello",
+            content: "World",
+          },
+          status: "published",
+        },
+        text: "Hello",
+      },
+    ],
+  });
+
+  const commentOnPost = new Mutation("CommentOnPost", {
+    input: new Input("CommentOnPost", {
+      fields: comment.config.fields,
+    }),
+    output: comment,
+    resolver: async (ctx) => [
+      {
+        author: {
+          email: "",
+          age: 20,
+        },
+        post: {
+          author: {
+            email: "test@test.com",
+            age: 20,
+          },
+          content: {
+            title: "Hello",
+            content: "World",
+          },
+          status: "published",
+        },
+        text: "Hello",
+      },
+    ],
+  });
+
+  const getComments = new Query("GetComments", {
+    input: new Input("GetComments", {
+      fields: {
+        post: new Field(post),
+      },
+    }),
+    output: comment,
+    resolver: async (ctx) => [
+      {
+        author: {
+          email: "",
+          age: 20,
+        },
+        post: {
+          author: {
+            email: "test@test.com",
+            age: 20,
+          },
+          content: {
+            title: "Hello",
+            content: "World",
+          },
+          status: "published",
+        },
+        text: "Hello",
+      },
+    ],
+  });
+
+  await t.step("creates a correct schema", async () => {
+    const schema = new Schema([
+      registerUser,
+      createContent,
+      postContent,
+      commentOnPost,
+      getComments,
+    ]);
+
+    const schemaString = schema.toString();
+    console.log(schemaString);
+    assert(schemaString);
+  });
 });
-
-// type UserInput = ResolvedFields<typeof createUser.fields>;
-// const userInput: UserInput = {
-//   username: "geoff",
-//   age: 5,
-// };
-
-const user = new Type("User", {
-  id: String,
-  username: String,
-  // email: String,
-  age: {
-    type: Int,
-    validator: (x) => typeof x === "number" && x > 18,
-    resolver: async (ctx) => [19],
-  },
-});
-
-// const content = new Type(
-//   "Content",
-//   {
-//     title: String,
-//     content: String,
-//   },
-//   (ids) => [
-//     {
-//       title: "Hello",
-//       content: "World",
-//     },
-//   ]
-// );
-
-// const post = new Type(
-//   "Post",
-//   {
-//     author: {
-//       type: user,
-//       resolver: (posts) => [user],
-//     },
-//     content: {
-//       type: user,
-//       resolver: (posts) => [user],
-//     },
-//     status: {
-//       type: String,
-//       validator: (item: string) => ["draft", "published"].includes(item),
-//     },
-//   },
-//   (ids) => [{}]
-// );
-
-// const comment = new Type(
-//   "Comment",
-//   {
-//     author: {
-//       type: user,
-//       resolver: (comments) => [user],
-//     },
-//     text: String,
-//   },
-//   (ids) => [{}]
-// );
-
-// const schema = new Schema([]);
