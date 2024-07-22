3c5d2bb038e4e90c2e5f5ba61270d4765770fb7b
Seb Ringrose <sebringrose@gmail.com>
2024-07-22 22:06:53 +0100

    feat: cleaned preactSSR example

---
+++ example/preactSSR/handlers/preact.handler.ts
@@ -0,0 +1,32 @@
+import { renderToString } from "preact-render-to-string";
+import { Handler, ssr } from "../../../mod.ts";
+import htmlTemplate from "../src/document.ts";
+
+export const preactHandler =
+  (
+    component: (props: Record<string, unknown>) => unknown,
+    title: string,
+    entrypoint: string
+  ): Handler =>
+  (ctx) => {
+    return ssr(
+      () => {
+        const ssrHTML = renderToString(component(ctx.state), null, null);
+        return htmlTemplate({
+          title,
+          ssrHTML,
+          entrypoint,
+          serverState: ctx.state,
+        });
+      },
+      {
+        headers: new Headers({
+          // instruct browser to cache page in prod env
+          "Cache-Control":
+            ctx.state.env.ENVIRONMENT === "production"
+              ? "max-age=86400, stale-while-revalidate=86400"
+              : "no-store",
+        }),
+      }
+    )(ctx);
+  };
