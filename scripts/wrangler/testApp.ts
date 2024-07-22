fee2bbdb8433d59f12b38f11f2f00ac37045aff8
Seb Ringrose <sebringrose@gmail.com>
2024-07-22 15:33:46 +0100

    feat: local dev pages

--- scripts/wrangler/testApp.ts
+++
@@ -1,9 +0,0 @@
-import { getTestRouter } from "../../tests/mocks/middleware.ts";
-
-const testRouter = getTestRouter();
-
-export default {
-  fetch(request: Request) {
-    return testRouter.handle(request);
-  },
-} satisfies ExportedHandler;
