dcf2cc0d8247e42ec11bd0d8c7ccedf67c4167d0
Sebastien Ringrose <seb.ringrose@pynea.com>
2024-07-22 16:08:04 +0100

    fix: bun script

--- scripts/bun/main.ts
+++ scripts/bun/main.ts
@@ -1,13 +1,14 @@
 import router from "../../example/preactSSR/router.ts";
 
 router.middleware.unshift((ctx) => {
-  ctx.state.env = process.env 
+  ctx.state.env = process.env;
 });
 
 Bun.serve({
-  port: 8080,
+  port: 7778,
   fetch(req) {
     return router.handle(req);
+  },
 });
 
 console.log("Bun server running with Peko router <3");
