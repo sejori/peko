6d9f0b1bec3daad4611fef92d6d4691f76008908
Sebastien Ringrose <seb.ringrose@pynea.com>
2024-07-26 15:53:27 +0100

    fix: cloudflare imports

--- example/reactSSR/handlers/github.handler.ts
+++ example/reactSSR/handlers/github.handler.ts
@@ -1,24 +1,23 @@
 import { Handler, RequestContext, file } from "../../../mod.ts";
 
-const base = `https://raw.githubusercontent.com/sejori/peko/main/example/reactSSR/`;
-
 export const githubHandler =
   (path: string, type?: string): Handler =>
-  async (ctx: RequestContext<{ env?: { ENVIRONMENT: string } }>) =>
-    (
-      await file(
-        ctx.state.env?.ENVIRONMENT === "production"
-          ? new URL(`${base}${path}`)
-          : new URL(".." + path, import.meta.url),
-        {
-          headers: new Headers({
-            ...(type && { "Content-Type": type }),
-            // instruct browser to cache file in prod env
-            "Cache-Control":
-              ctx.state.env?.ENVIRONMENT === "production"
-                ? "max-age=86400, stale-while-revalidate=86400"
-                : "no-store",
-          }),
-        }
-      )
+  async (ctx: RequestContext<{ env?: { ENVIRONMENT: string } }>) => {
+    const base =
+      ctx.state.env?.ENVIRONMENT === "production" || !import.meta.url
+        ? `https://raw.githubusercontent.com/sejori/peko/main/example/reactSSR/`
+        : import.meta.url.replace("handlers/github.handler.ts", "");
+
+    return (
+      await file(new URL(`${base}${path}`), {
+        headers: new Headers({
+          ...(type && { "Content-Type": type }),
+          // instruct browser to cache file in prod env
+          "Cache-Control":
+            ctx.state.env?.ENVIRONMENT === "production"
+              ? "max-age=86400, stale-while-revalidate=86400"
+              : "no-store",
+        }),
+      })
     )(ctx);
+  };
