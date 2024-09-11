e991e46d7cdfb53f1716c19945a9ce02439cfb73
Sebastien Ringrose <seb.ringrose@pynea.com>
2024-09-11 11:04:08 +0100

    fix: examples

--- example/reactSSR/handlers/react.handler.ts
+++ example/reactSSR/handlers/react.handler.ts
@@ -1,17 +1,18 @@
+import { JSX, ReactNode } from "react";
 import { renderToString } from "react-dom/server";
 import { Handler, RequestContext, ssr } from "../../../mod.ts";
 import htmlTemplate from "../src/document.ts";
 
 export const reactHandler =
   (
-    component: (props: Record<string, unknown>) => unknown,
+    component: (props: Record<string, ReactNode>) => JSX.Element,
     title: string,
     entrypoint: string
   ): Handler =>
   (ctx: RequestContext<{ env?: { ENVIRONMENT: string } }>) => {
     return ssr(
       () => {
-        const ssrHTML = renderToString(component(ctx.state), null);
+        const ssrHTML = renderToString(component(ctx.state as Record<string, ReactNode>));
         return htmlTemplate({
           title,
           ssrHTML,
---
+++ scripts/deno/auth.ts
@@ -0,0 +1,15 @@
+import router from "../../example/singleFileAuth/app.ts";
+
+router.middleware.unshift((ctx) => {
+  ctx.state.env = Deno.env.toObject();
+});
+
+// Start Deno server with Peko router :^)
+Deno.serve(
+  {
+    port: 7777,
+  },
+  (req) => router.handle(req)
+);
+
+console.log("Deno server running with Peko router <3");
